21cdc9e23edeb2369e4c1c3d2b7923b0ebebe439
Matt Enyeart <menyeart1@gmail.com>
2024-01-12 10:35:31 -0700

    Create test for average age class method

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -4,4 +4,23 @@ RSpec.describe Contestant, type: :model do
   describe 'relationships' do
     it {should belong_to :bachelorette}
   end
+
+  describe "methods" do
+    it "can determine the average age of all contestants" do
+      jenny = Bachelorette.create(name: "Jenny", season_number: 1)
+      bill = Contestant.create(name: "Bill", age: 10, hometown: "Los Angeles", bachelorette_id: jenny.id)
+      jomah = Contestant.create(name: "Jomah", age: 20, hometown: "Denver", bachelorette_id: jenny.id)
+      merry = Contestant.create(name: "Bill", age: 30, hometown: "Deluth", bachelorette_id: jenny.id)
+
+      expect(jenny.contestants.average_age).to eq(20)
+
+      jill = Contestant.create(name: "Jill", age: 40, hometown: "Deluth", bachelorette_id: jenny.id)
+
+      expect(jenny.contestants.average_age).to eq(25)
+
+      jill = Contestant.create(name: "Jill", age: 40, hometown: "Deluth")
+
+      expect(jenny.contestants.average_age).to eq(25)
+    end 
+  end
 end
