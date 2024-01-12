4151b0b2f1b23934c5c39209fd5d8f132c74ff4a
Matt Enyeart <menyeart1@gmail.com>
2024-01-12 08:45:46 -0700

    Changed the naming convention to match nested resources

---
+++ spec/features/bachelorettes/contestants/index_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Bachelorette Contestants Index Page", type: :feature do
+  describe "As a visitor" do
+    it "When I visit a bachelorette's contestants index, I see the names of that bachelorette's contestants along with the their age, hometown and link to their respective show pages" do
+      lauren = Bachelorette.create(name: "Lauren", season_number: 1)
+
+      rachel = Contestant.create(name: "rachel", age: 35, hometown: "Los Angeles", bachelorette_id: lauren.id)
+      merry = Contestant.create(name: "merry", age: 31, hometown: "Des Moines", bachelorette_id: lauren.id)
+      sunita = Contestant.create(name: "sunita", age: 40, hometown: "Mumbai", bachelorette_id: lauren.id)
+
+      visit bachelorette_contestants_path(lauren.id)
+
+      expect(page).to have_content(rachel.name)
+      expect(page).to have_content(rachel.hometown)
+      expect(page).to have_content(rachel.age)
+      expect(page).to have_content(merry.name)
+      expect(page).to have_content(merry.hometown)
+      expect(page).to have_content(merry.age)
+      expect(page).to have_content(sunita.name)
+      expect(page).to have_content(sunita.hometown)
+      expect(page).to have_content(sunita.age)
+    end
+  end
+end
---
+++ spec/features/bachelorettes/show_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe "Bachelorette Show Page", type: :feature do
+  describe "As a visitor" do
+    it "when I visit a bachelorette's show page I see their name, season number and description as well as a link to their contestants" do
+      lauren = Bachelorette.create(name: "Lauren", season_number: 1)
+
+      rachel = Contestant.create(name: "rachel", age: 35, hometown: "Los Angeles", bachelorette_id: lauren.id)
+      merry = Contestant.create(name: "merry", age: 31, hometown: "Des Moines", bachelorette_id: lauren.id)
+      sunita = Contestant.create(name: "sunita", age: 40, hometown: "Mumbai", bachelorette_id: lauren.id)
+
+      visit "/bachelorettes/#{lauren.id}"
+
+      expect(page).to have_content("Bachelorette: Lauren")
+      expect(page).to have_content("Season: 1")
+      expect(page).to have_link("Lauren's Suitors")
+
+      click_link("Lauren's Suitors")
+    
+      expect(current_path).to eq("/bachelorettes/#{lauren.id}/contestants")
+      expect(page).to have_content("rachel")
+      expect(page).to have_content("merry")
+      expect(page).to have_content("sunita")
+    end
+  end
+end
